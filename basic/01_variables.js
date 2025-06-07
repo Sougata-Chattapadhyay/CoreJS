a62fe019f47e758491fdbc54ce6367a9fbe029d3
Sougata-Chattapadhyay <sougata2k20@gmail.com>
2025-06-07 18:21:09 +0530

    dataType

---
+++ basic/01_variables.js
@@ -0,0 +1,11 @@
+console.log("Hello world");
+let email="a@a.com"
+let name="a"
+let password="password"
+let accountStatus;
+/*
+Prefer not to use var
+because of issue in block scope or functional scope
+
+*/
+console.table([name,email,password,accountStatus])
---
+++ basic/02_dataType.js
@@ -0,0 +1,20 @@
+"use strict"; //trest all JS code as newer version
+
+// alert(3+3) //we are using node.js, not browser
+
+/*
+primitive datatype
+================================
+number => 2 to power 53
+bigInt
+string => ""
+boolean => true/false
+null => standalone value
+undefined
+symbol
+
+
+*/
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
